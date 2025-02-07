2a763e6249b04725eb29ca4dc83bdd756f7fab04
Raj <rpdinkar@gmail.com>
2025-02-07 10:55:59 +0530

    user changes

--- app/page.js
+++ app/page.js
@@ -157,6 +157,7 @@ export default function LandingPage() {
         </div>
       </section>
 
+
       {/* FAQ Section */}
       <section className="w-full py-12 md:py-24">
         <div className="container mx-auto px-4 md:px-6">
